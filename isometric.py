19a1c7a2462c2f09dfa305de7d59b63e6cf7abaa
Naman Biyani <namanb@iitk.ac.in>
2019-04-11 19:02:22 +0530

    Add files via upload

---
+++ isometric.py
@@ -0,0 +1,19 @@
+#Before running the code, open MS paint in background with pencil/pen tool selected.
+import time 
+import math as m
+import pyautogui
+
+time.sleep(5)
+pyautogui.moveTo(1000,800)
+
+pyautogui.dragRel(200*m.cos(m.radians(30),(-200)*m.sin(m.radians(30)),duration=1)
+pyautogui.dragRel(0,-200,duration=1)
+pyautogui.dragRel((-200)*m.cos(m.radians(30),200*m.sin(m.radians(30)),duration=1)
+pyautogui.dragRel(0,200,duration=1)
+pyautogui.dragRel((-200)*m.cos(m.radians(30),(-200)*m.sin(m.radians(30)),duration=1)
+pyautogui.dragRel(0,-200,duration=1)
+pyautogui.dragRel(200*m.cos(m.radians(30),200*m.sin(m.radians(30)),duration=1)
+pyautogui.moveRel(200*m.cos(m.radians(30),(-200)*m.sin(m.radians(30)),duration=1)
+pyautogui.dragRel((-200)*m.cos(m.radians(30),(-200)*m.sin(m.radians(30)),duration=1)
+pyautogui.dragRel((-200)*m.cos(m.radians(30),200*m.sin(m.radians(30)),duration=1)
+
